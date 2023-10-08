90dde9ee848aac81071e74919a42654b8a3403d5
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-08 18:02:22 +0900

    feat(ex00): implement ex00
    - implement ScalarConverter class
    - implement main function

---
+++ ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 13:28:20 by yena              #+#    #+#             */
+/*   Updated: 2023/10/08 17:03:09 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "ScalarConverter.hpp"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+	std::cout << "Error: Wrong number of arguments" << std::endl;
+  }
+  else {
+	std::string str = (std::string)argv[1];
+	ScalarConverter::convert(str);
+  }
+  return 0;
+}
