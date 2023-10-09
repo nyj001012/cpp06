dade969d8c8936279f4cdfb38f6cf7735a085801
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-09 16:55:55 +0900

    feat(ex02): implement ex02

---
+++ ex02/C.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 나예진 on 2023/10/09.
+//
+
+#include "C.hpp"
---
+++ ex02/main.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 15:24:25 by yena              #+#    #+#             */
+/*   Updated: 2023/10/09 16:55:06 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *generate() {
+  srand(time(NULL));
+  int random = rand() % 3;
+  if (random == 0)
+	return new A();
+  else if (random == 1)
+	return new B();
+  else
+	return new C();
+}
+
+void identify(Base *p) {
+  std::cout << "------------------- IDENTIFY *p -------------------" << std::endl;
+  if (dynamic_cast<A*>(p))
+	std::cout << "type: A" << std::endl;
+  if (dynamic_cast<B*>(p))
+	std::cout << "type: B" << std::endl;
+  if (dynamic_cast<C*>(p))
+	std::cout << "type: C" << std::endl;
+}
+
+void identify(Base &p) {
+  std::cout << "------------------- IDENTIFY &p -------------------" << std::endl;
+  try {
+	(void)dynamic_cast<A&>(p);
+	std::cout << "type: A" << std::endl;
+  } catch (std::exception &e) {
+	try {
+	  dynamic_cast<B&>(p);
+	  std::cout << "type: B" << std::endl;
+	} catch (std::exception &e) {
+	  try {
+		dynamic_cast<C&>(p);
+		std::cout << "type: C" << std::endl;
+	  } catch (std::exception &e) {
+		std::cout << "Error: unknown type" << std::endl;
+	  }
+	}
+  }
+}
+
+int main() {
+  Base *base = generate();
+  Base &baseRef = *base;
+
+  identify(base);
+  identify(baseRef);
+  delete base;
+
+  return 0;
+}
